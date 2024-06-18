b0b2d3d0bca8a53a64eee4a8367cbede913edb56
Digvijay Patil <digvijaypatil8699@gmail.com>
2024-06-18 19:58:56 +0530

    Commiting the shouldCreateStoreTable test method.

--- src/test/java/com/example/Store_app_Rest_api/StoreAppRestApiApplicationTests.java
+++ src/test/java/com/example/Store_app_Rest_api/StoreAppRestApiApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.Store_app_Rest_api;
 
+import com.example.Store_app_Rest_api.dto.ResponseStore;
+import com.example.Store_app_Rest_api.entity.Store;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class StoreAppRestApiApplicationTests {
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void shoudCreateStoreTable(){
+		Store store = new Store(1,"Raman","Pune",234455.76);
+		ResponseEntity<ResponseStore> response = testRestTemplate
+				.postForEntity("http://localhost:" + port +"/addItem",store,ResponseStore.class);
+
+		Assertions.assertEquals(201,response.getStatusCode().value());
+		ResponseStore body = response.getBody();
+		Assertions.assertNotNull(body);
+		Assertions.assertEquals("Data Store Successfully!",body.getMessage());
+		Assertions.assertEquals(HttpStatus.CREATED,body.getHttpStatus());
+
+	}
+
 
 }
